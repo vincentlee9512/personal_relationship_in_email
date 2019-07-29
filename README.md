42c896eb43fede5c44856ebe8697c8119ca4d18e
vincentlee9512 <li000053@stthomas.edu>
2019-07-29 11:17:51 +0800

    上传数据集

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # personal_relationship_in_email
-用 PageRank 算法分析邮件里存在的人际关系
+李文轩 2019-07-29
+
+##### 项目：用 PageRank 算法分析邮件里存在的人际关系
+
+
---
+++ pr_in_email.py
@@ -0,0 +1,11 @@
+"""
+Author: 李文轩
+
+邮件中的人际关系 - 用 PageRank 算法进行分析
+
+数据：https://github.com/vincentlee9512/personal_relationship_in_email/input
+"""
+
+
+import networkx as nx
+
